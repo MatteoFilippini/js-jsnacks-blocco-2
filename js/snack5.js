85fdbc3afd5aa688fa557f55af5248fc386314b5
MatteoFilippini <matteofilippini8@gmail.com>
2022-01-13 16:15:31 +0100

    created snack 5

---
+++ js/snack5.js
@@ -0,0 +1,13 @@
+/*
+Crea due array che hanno un numero di elementi diversi.
+Aggiungi elementi casuali all’array che ha meno elementi,
+fino a quando ne avrà tanti quanti l’altro.
+*/
+ 
+// CREO UN NUMERO RANDOM
+// CREO 2 ARRAY DI LUNGHEZZA RANDOM     
+// CONTROLLO ARRAY PIU CORTO
+// RIEMPIO FINO CON A QUANTO NON HANNO LA STESSA LUNGHEZZA
+    // CREO ELEMENTO RANDOM
+    // RIEMPIO
+// STAMPO
