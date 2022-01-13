0678267da48aab14c54c6d9013d3e1425bf9933f
MatteoFilippini <matteofilippini8@gmail.com>
2022-01-13 15:26:02 +0100

    road map

---
+++ js/snack3.js
@@ -0,0 +1,12 @@
+/*
+Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una
+lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
+*/
+
+// CREO UNA LISTA DI NOMI E COGNOMI
+// CREO LA LISTA DI GATSBY
+// FINO A QUANDO LA LISTA DI GATSBY NON HA 3 NOMICOGNOMI DIVERSI LA RIEMPIO
+    // PRENDO UN NOME E UN COGNOME A CASO
+    // CONTROLLO CHE NON SIA IL LISTA
+    // LI METTO IN LISTA
+// STAMPO 
