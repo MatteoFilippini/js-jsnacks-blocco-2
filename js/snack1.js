0233e7b1ca4a78baa66e685376c0308e548ef284
MatteoFilippini <matteofilippini8@gmail.com>
2022-01-13 14:57:52 +0100

    created varibles

--- js/snack1.js
+++ js/snack1.js
@@ -1,1 +1,9 @@
-console.log('fd0');
+/*Il software deve chiedere per 5 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+Esegui questo programma in due versioni, con il for e con il while.*/
+
+// CREO LE VARIABILI NUM,SUM
+const num = prompt('Inserisci un numero', 5).trim();
+let sum = 0;
+
+// OGNI VOLTA SOMMO IL NUMERO ALLA SOMMA PER 5 VOLTE
