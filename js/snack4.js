afd1ee66a4d1ca30861c0120c179493fe3d77951
MatteoFilippini <matteofilippini8@gmail.com>
2022-01-13 16:08:29 +0100

    checked sum and numbers && printed

--- js/snack4.js
+++ js/snack4.js
@@ -4,6 +4,15 @@ e fai la somma di tutti gli elementi che sono in posizione dispari
 */
 
 // CREO L'ARRAY
+const num = [1, 0, 4, 9];
 // CONTROLLO CHE NON SIANO NELLA POSIZIONE PARI
-// SOMMO
+let sum = 0;
+for (let i = 0; i < num.length; i++) {
+    if (i % 2 === 0) {
+        // SOMMO
+        sum += num[i];
+    }
+}
 // STAMPO
+console.log(num);
+console.log(sum);
