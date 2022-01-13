3ca6a9a9bcdb9412f2461be04283a64cb9e279f7
MatteoFilippini <matteofilippini8@gmail.com>
2022-01-13 15:00:18 +0100

    parseInt variable num && printed sum

--- js/snack1.js
+++ js/snack1.js
@@ -3,7 +3,14 @@ Il programma stampa la somma di tutti i numeri inseriti.
 Esegui questo programma in due versioni, con il for e con il while.*/
 
 // CREO LE VARIABILI NUM,SUM
-const num = prompt('Inserisci un numero', 5).trim();
+
 let sum = 0;
 
 // OGNI VOLTA SOMMO IL NUMERO ALLA SOMMA PER 5 VOLTE
+for (let i = 0; i < 5; i++) {
+    const num = parseInt(prompt('Inserisci un numero', 5).trim());
+    sum += num;
+}
+
+// STAMPO LA SOMMA
+console.log(sum);
