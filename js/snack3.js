b58f55c5288ac044828fdf2f773bdeaa0e6e10a1
MatteoFilippini <matteofilippini8@gmail.com>
2022-01-13 15:28:14 +0100

    created arrays

--- js/snack3.js
+++ js/snack3.js
@@ -4,6 +4,8 @@ lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
 */
 
 // CREO UNA LISTA DI NOMI E COGNOMI
+const names = ['Matteo', 'Luca', 'Roberta',]
+const surnames = ['Filippini', 'Garda'];
 // CREO LA LISTA DI GATSBY
 // FINO A QUANDO LA LISTA DI GATSBY NON HA 3 NOMICOGNOMI DIVERSI LA RIEMPIO
     // PRENDO UN NOME E UN COGNOME A CASO
