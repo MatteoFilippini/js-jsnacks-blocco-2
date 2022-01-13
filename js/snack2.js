36402f9f83715701ee6105fa15f91c2758893142
MatteoFilippini <matteofilippini8@gmail.com>
2022-01-13 15:16:12 +0100

    created variable num

---
+++ js/snack2.js
@@ -0,0 +1,9 @@
+/*
+Inserisci un numero, se è pari stampa il numero,
+se è dispari stampa il numero successivo
+*/
+
+// CHIEDO IL NUMERO
+let num=parseInt(prompt('Inserisci un numero'));
+// SE NON E DISPARI AGGIUNGO 1 
+// STAMPO
