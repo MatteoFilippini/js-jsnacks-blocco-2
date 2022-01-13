87c8984c3d0e177c33bccc09bff87c69930cccd1
MatteoFilippini <matteofilippini8@gmail.com>
2022-01-13 15:12:15 +0100

    added while

--- js/snack1.js
+++ js/snack1.js
@@ -3,13 +3,23 @@ Il programma stampa la somma di tutti i numeri inseriti.
 Esegui questo programma in due versioni, con il for e con il while.*/
 
 // CREO LE VARIABILI NUM,SUM
-let sum = 0;
+let sumFor = 0;
+let sumWhile = 0;
+let i = 1;
+
+// FINO A QUANDO NON ARRIVO A 5 NUMERI CHIEDI UN NUMERO E SOMMA
+while (i <= 5) {
+    const num = parseInt(prompt(`Inserisci il ${i} numero`, 5).trim());
+    sumWhile += num;
+    i++;
+}
 
 // OGNI VOLTA SOMMO IL NUMERO ALLA SOMMA PER 5 VOLTE
-for (let i = 1; i <= 5; i++) {
-    const num = parseInt(prompt(`Inserisci il ${i} numero`, 5).trim());
-    sum += num;
+for (let k = 1; k <= 5; k++) {
+    const num = parseInt(prompt(`Inserisci il ${k} numero`, 5).trim());
+    sumFor += num;
 }
 
 // STAMPO LA SOMMA
-console.log(sum);
+console.log("somma-for" + sumFor);
+console.log("somma-while" + sumWhile);
