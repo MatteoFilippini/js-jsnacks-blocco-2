5674e8e6e8bfc9f1b119a086f552d854aaf18042
MatteoFilippini <matteofilippini8@gmail.com>
2022-01-13 16:09:48 +0100

    updated check

--- js/snack4.js
+++ js/snack4.js
@@ -7,11 +7,14 @@ e fai la somma di tutti gli elementi che sono in posizione dispari
 const num = [1, 0, 4, 9];
 // CONTROLLO CHE NON SIANO NELLA POSIZIONE PARI
 let sum = 0;
-for (let i = 0; i < num.length; i++) {
-    if (i % 2 === 0) {
-        // SOMMO
-        sum += num[i];
-    }
+// for (let i = 0; i < num.length; i++) {
+//     if (i % 2 === 0) {
+//         // SOMMO
+//         sum += num[i];
+//     }
+// }
+for (let i = 0; i < num.length; i += 2) {
+    sum += num[i];
 }
 // STAMPO
 console.log(num);
