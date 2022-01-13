e5b5d1646a6c94e1a92466e79d6e968c8a9b8665
MatteoFilippini <matteofilippini8@gmail.com>
2022-01-13 16:17:29 +0100

    created random array

--- js/snack5.js
+++ js/snack5.js
@@ -3,9 +3,12 @@ Crea due array che hanno un numero di elementi diversi.
 Aggiungi elementi casuali all’array che ha meno elementi,
 fino a quando ne avrà tanti quanti l’altro.
 */
- 
+
 // CREO UN NUMERO RANDOM
-// CREO 2 ARRAY DI LUNGHEZZA RANDOM     
+const n = Math.floor(Math.random() * 10);
+// CREO 2 ARRAY DI LUNGHEZZA RANDOM    
+const array1 = [n];
+const array2 = [n + 3];
 // CONTROLLO ARRAY PIU CORTO
 // RIEMPIO FINO CON A QUANTO NON HANNO LA STESSA LUNGHEZZA
     // CREO ELEMENTO RANDOM
