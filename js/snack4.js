176db4fbe3acb250e4a9cfd805c1ff663c0b403b
MatteoFilippini <matteofilippini8@gmail.com>
2022-01-13 15:57:24 +0100

    created road map

---
+++ js/snack4.js
@@ -0,0 +1,9 @@
+/*
+Crea un array di numeri interi
+e fai la somma di tutti gli elementi che sono in posizione dispari
+*/
+
+// CREO L'ARRAY
+// CONTROLLO CHE NON SIANO NELLA POSIZIONE PARI
+// SOMMO
+// STAMPO
