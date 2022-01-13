9b3e72845662d86365dded4088af247390703ceb
MatteoFilippini <matteofilippini8@gmail.com>
2022-01-13 15:39:24 +0100

    filled gatsby list

--- js/snack3.js
+++ js/snack3.js
@@ -11,14 +11,16 @@ const gatsby = [];
 // FINO A QUANDO LA LISTA DI GATSBY NON HA 3 NOMICOGNOMI DIVERSI LA RIEMPIO
 let i = 0;
 let nameSurname;
-while (i < 3) {
+while (i <= 3) {
     // PRENDO UN NOME E UN COGNOME A CASO
     const randomName = Math.floor(Math.random() * names.length);
     const randomSurname = Math.floor(Math.random() * surnames.length);
-    nameSurname = `${names[randomName]} ${surnames[randomSurname]}`;
+    nameSurname = `${names[randomName]} ${surnames[randomSurname]}`; 
+    // LI METTO IN LISTA
+    gatsby[i]=nameSurname;
     i++;
 }
-console.log(nameSurname)
+console.table(gatsby)
     // CONTROLLO CHE NON SIA IL LISTA
-    // LI METTO IN LISTA
+    
 // STAMPO 
