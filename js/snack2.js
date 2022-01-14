0d422ca95d2069b21bc26af68d075c444b3555af
MatteoFilippini <matteofilippini8@gmail.com>
2022-01-14 09:43:47 +0100

    update snack 2 do-while

--- js/snack2.js
+++ js/snack2.js
@@ -3,11 +3,16 @@ Inserisci un numero, se è pari stampa il numero,
 se è dispari stampa il numero successivo
 */
 
-// CHIEDO IL NUMERO
-let num = parseInt(prompt('Inserisci un numero'));
+// CHIEDO IL NUMERO FINTNTANTO CHE E UN NUMERO
+let num;
+do {
+    num = parseInt(prompt('Inserisci un numero'));
+} while (isNaN(num))
 // SE NON E DISPARI AGGIUNGO 1 
 if (!(num % 2 === 0)) {
     num++;
 }
 // STAMPO
 console.log(num);
+
+
